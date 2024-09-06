55c61349511d6957ed093313b3bb5c7746d84797
Bhavesh Negi <bhaveshnegi98@gmail.com>
2024-09-06 13:03:53 +0530

    Add loginpage with validation

---
+++ front-end/src/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    const [logindata,setLoginData]=useState({
+        username:'',
+        password:'',
+    })
+
+    const handleLoginChange=(e)=>{
+        const {name,value} = e.target;
+        setLoginData((prevData)=>({
+            ...prevData,
+            [name]:value
+        }))
+    }
+
+    const handleLoginSubmit=(e)=>{
+        e.preventDefault()
+    }
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleLoginSubmit}>
+        <input type='text' name='username' placeholder='username' value={logindata.username}required onChange={handleLoginChange}/>
+        <input type='password' name='password' placeholder='Password' value={logindata.password}required onChange={handleLoginChange}/>
+        <button type='submit'>Login</button>
+        <p>
+            Not register yet?
+            <Link to ='/register'>Register here </Link>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Login
