60efac3bd74b1bf9a10d5cbdc7e28c6368deb7b0
Bhavesh Negi <bhaveshnegi98@gmail.com>
2024-09-06 23:00:14 +0530

    add registeration page

--- front-end/src/Register.js
+++ front-end/src/Register.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [registerData,setRegisterData]= useState({
+    username:'',
+    password:''
+  })
+  const handleRegisterChange=(e)=>{
+    const {name,value} = e.target;
+    setRegisterData((preData)=>({
+      ...preData,
+      [name]:value,
+    }))
+  }
+
+  const handleRegisterSubmit=async(e)=>{
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8000/register',registerData);
+      console.log(response.data);
+    } 
+    catch (error) {
+      console.log(error);
+    }
+    setRegisterData({
+      username:'',
+      password:'',
+    })
+  }
   return (
     <div>
-      
+      <h1>Register Page</h1>
+      <form onSubmit={handleRegisterSubmit}>
+        <label>Username:</label>
+        <input type='text' name='username' placeholder='enter username' value={registerData.username} onChange={handleRegisterChange} required/>
+        <label>Password:</label>
+        <input type='password' name='password' placeholder='enter password' value={registerData.password} onChange={handleRegisterChange} required/>
+        <button type='submit'>Register</button>
+        <p>Already Register
+          <Link to='/login'>Login Here</Link>
+        </p>
+      </form>
     </div>
   )
 }
